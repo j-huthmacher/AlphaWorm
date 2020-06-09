67000f9e97b63bd2bc9153040ccb966799d42c19
Richard Poschinger <richard@poschinger.net>
2020-06-09 17:27:43 +0200

    Added initial version of deployment script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get install sshpass rsync -y
+
+echo "--Please enter IFI-CIP username (without @ and suffix)"
+read username
+address="${username}@remote.cip.ifi.lmu.de"
+echo "--Enter IFI-CIP Password"
+read -s password
+echo "--Please enter project target directory name"
+read dir
+full_dir="/work/${dir}/"
+
+sshpass -p ${password} rsync -av --update ./PythonDev/* ${address}:~${full_dir}
+sshpass -p ${password} rsync -av --update ./start.sh ${address}:~${full_dir}
+sshpass -p ${password} rsync -av --update ./envs/* ${address}:~${full_dir}/envs/
+
+sshpass -p ${password} ssh ${address} "sed -i 's|REPLACE|.$full_dir|g' .${full_dir}/start.sh"
+sshpass -p ${password} ssh ${address} sbatch --partition=All --cpus-per-task=4 ./work/${dir}/start.sh
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd REPLACE
+pip3 install gym_unity mlagents baselines
+python3 training.py
